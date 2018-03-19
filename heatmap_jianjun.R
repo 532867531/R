025b2364087e8787bb4e20218fd6bd0cfb600702
532867531 <532867531@qq.com>
2018-03-19 23:21:46 +0800

    调整可以用的，break参数自动生成！匹配

--- heatmap_jianjun.R
+++ heatmap_jianjun.R
@@ -1,11 +1,17 @@
 library(matrixStats)
 library(NMF)
-
+setwd("D:\\minÊý¾Ý\\tcga\\prad_tcga\\analyse\\")
 a<-read.csv(file="tcga_signature.csv",h=T,as.is=T,row.names=1)
 a<-log2(a+1)
 b<-a-rowMedians(data.matrix(a))
 
+
+library(lattice)
 d1<-b
-aheatmap(d1,color=colorRampPalette(c("blue", "white", "red"))(802), distfun="euclidean",breaks=c(min(d1)-0.05,seq(-4,4,0.05),max(d1)+0.05),hclustfun="complete",scale="none",labCol=NA,legend=T)
+win.graph();densityplot(unlist(d1))
+quantile(unlist(d1),0.99)
+win.graph();aheatmap(d1,color=colorRampPalette(c("blue", "white", "red"))(800), distfun="euclidean",
+          # breaks=c(-1,1),
+           hclustfun="complete",scale="none",labCol=NA,legend=T)
 d2<-t(scale(t(b)))
 aheatmap(d2,color=colorRampPalette(c("blue", "white", "red"))(802), distfun="euclidean",breaks=c(min(d2)-0.05,seq(-2,2,0.005),max(d2)+0.05),hclustfun="complete",scale="none",labCol=NA,legend=T)
